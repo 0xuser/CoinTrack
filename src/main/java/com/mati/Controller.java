b88f08c20a6aa6638aa9f2bd928733bd44fb7bfc
0xmati <legalneklucze@gmail.comgit config --global user.email legalneklucze@gmail.comgit config --global user.email legalneklucze@gmail.com>
2019-10-11 14:09:11 +0200

    added ui

---
+++ src/main/java/com/mati/Controller.java
@@ -0,0 +1,22 @@
+package com.mati;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    public void handle_button_settings(){
+
+    }
+    public void handle_button_refresh(){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
